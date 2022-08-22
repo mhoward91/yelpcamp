24292911f6ef7f8dccf2cb59d811b645a9de12a4
Matt Howard <matt.howard91@gmail.com>
2022-08-22 12:19:34 +0100

    pre image upload

--- middleware.js
+++ middleware.js
@@ -33,8 +33,8 @@ module.exports.isAuthor = async (req, res, next) => {
 }
 
 module.exports.isReviewAuthor = async (req, res, next) => {
-    const { reviewId } = req.params
-    const review = await Campground.findById(reviewId)
+    const { id, reviewId } = req.params
+    const review = await Review.findById(reviewId)
     if(!review.author.equals(req.user._id)) {
         req.flash("error", "You don't have permission to do that!")
         return res.redirect(`/campgrounds/${id}`)
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,37 +2,35 @@ const express = require("express")
 const router = express.Router()
 const catchAsync = require("../utils/catchAsync")
 const Campground = require("../models/campground")
+const campgrounds = require("../controllers/campgrounds")
 const ExpressError = require("../utils/expressError")
 const { campgroundSchema } = require("../schemas")
 const { isLoggedIn, validateCampground, isAuthor } = require("../middleware")
 
+// note callback functions are named in below routes only and defined within the `controllers` dir
+// router.route from express used to chain different request types to the same route (e.g. a GET and POST to "/")
 
-router.get("/", catchAsync(async (req, res) => {
-    const campgrounds = await Campground.find({})
-    res.render("campgrounds/index", { campgrounds })
-}))
+// the catchAsync utility fn will pass any errors to the error handling middleware 
+// use of third party library joi to do json payload validation on the server side
+// invoked through validateCamground middleware function
 
-router.get("/new", isLoggedIn, (req, res) => {
-    res.render("campgrounds/new")
-})
+router.route("/")
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
 
-router.get("/:id", async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate({
-        path: "reviews",
-        populate: {
-            path: "author"
-        }
-    }).populate("author")
-    if (!campground) {
-        req.flash("error", "That campground doesn't exist!")
-        res.redirect("/campgrounds")
-    } else {
-        res.render("campgrounds/show", { campground })
-    }
+router.get("/new", isLoggedIn, campgrounds.renderNewForm)
 
-})
+router.route("/:id")
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
 
-// // next param is required to call the next middleware function (in this case an error handling middleware function)
+
+router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
+
+module.exports = router
+
+// // the next param is required to call the next middleware function (in this case an error handling middleware function)
 // // alternative version below shows use of a wrapper function, avoiding using try & catch each time
 // app.post("/campgrounds", async (req, res, next) => {
 //     try {
@@ -43,40 +41,3 @@ router.get("/:id", async (req, res) => {
 //         next(e)
 //     }
 // })
-
-// the catchAsync utility fn will pass any errors to the error handling middleware 
-// use of third party library joi to do json payload validation on the server side
-// invoked through validateCamground middleware function
-router.post("/", isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
-    const campground = new Campground(req.body.campground)
-    campground.author = req.user._id
-    await campground.save()
-    req.flash("success", "Successfully made a new campground!")
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
-    if (!campground) {
-        req.flash("error", "That campground doesn't exist!")
-        res.redirect("/campgrounds")
-    } else {
-        res.render("campgrounds/edit", { campground })
-    }
-}))
-
-router.put("/:id", isLoggedIn, isAuthor, validateCampground, catchAsync(async (req, res) => {
-    const { id } = req.params
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
-    req.flash("success", "Successfully updated campground!")
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-router.delete("/:id", isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-    const { id } = req.params
-    await Campground.findByIdAndDelete(id)
-    req.flash("success", "Successfully deleted campground!")
-    res.redirect("/campgrounds")
-}))
-
-module.exports = router
